34704130fcbac3f1f7d337d6fdacad169a31b6cd
Irfandwi172 <irfandwiarfianto@gmail.com>
2026-01-01 13:09:02 +0700

    Add render build script with execute permission

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+composer install --no-dev --working-dir=/opt/render/project/src
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
