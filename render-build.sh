448bbf771b1dffe9cfc02b94fbd6652c6363be56
Irfandwi172 <irfandwiarfianto@gmail.com>
2026-01-01 13:34:10 +0700

    Update build script for SQLite

--- render-build.sh
+++ render-build.sh
@@ -3,6 +3,10 @@
 set -o errexit
 
 composer install --no-dev --working-dir=/opt/render/project/src
+
+# Create SQLite database
+touch /opt/render/project/src/database/database.sqlite
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
